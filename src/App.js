896f2cb1a6f4ad91fa161a547105cc62c424668d
Dmitry <dp_140482@mail.ru>
2021-10-30 11:21:18 +0300

    ДЗ 2, пункты 1 - 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Message } from './components/Message/Message';
+import { Form } from './components/Form/Form';
 import './App.css';
 
-const msgText = 'Моё первое сообщение.';
-
 const App = () => {
+  const [messageList, setMessageList] = useState([]);
+  const bottomPanel = useRef(null);
+  const scrollToBottom = () => {
+    bottomPanel.current?.scrollIntoView({ behavior: "auto" });
+  }
+
+  const handleFormSendMessage = (text) => {
+    const newMsg = {author: 'Вы', text: text};
+    setMessageList([...messageList, newMsg]);
+  }
+
+  useEffect( () => {
+    if (messageList.length > 0) {
+      if (messageList[messageList.length - 1].author === 'Вы') {
+        const newMsg = {author: 'Бот', text: 'Оператор ответит Вам позже.'};
+        setMessageList([...messageList, newMsg]);
+      }
+    }
+  }, [messageList]);
+
+  useEffect(() => {
+    scrollToBottom();
+  });
+
   return (
     <div className="app">
       <h1 className="app-header">Чат</h1>
-      <p>Сообщения:</p>
-      <Message text={msgText} />
+      <div className="app-content">
+        {messageList.map(mes => <Message msg={mes} />)}
+      </div>
+      <Form change={handleFormSendMessage} className="app-footer" />
+      <div ref={bottomPanel} />
     </div>
   );
 }
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import './Message.css';
 
-export const Message = ({text}) => {
+export const Message = ({msg}) => {
+    if (msg.author === 'Бот') {
+        return (
+            <div className="msg botmsg">
+                <p className="msg-head botmsg-head">{msg.author}</p>
+                <p className="msg-text botmsg-text">{msg.text}</p>
+            </div>
+        );
+    }
     return (
         <div className="msg">
-            <p className="msg-text">{text}</p>
+            <p className="msg-head humanmsg-head">{msg.author}</p>
+            <p className="msg-text">{msg.text}</p>
         </div>
     );
 }
