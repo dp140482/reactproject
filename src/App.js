1fa9480f0160ff3c11137eb2ec688a5ffba5972f
Dmitry <dp_140482@mail.ru>
2021-10-31 12:30:35 +0300

    Добавление key для сообщений

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Message } from './components/Message/Message';
 import { Form } from './components/Form/Form';
 import './App.css';
@@ -6,16 +6,23 @@ import './App.css';
 const App = () => {
   const [messageList, setMessageList] = useState([]);
 
+  const lastMessage = useCallback(() => {
+    if (messageList.length === 0) {
+      return {id: 0, author: '', text: ''};
+    }
+    return messageList[messageList.length - 1];
+  }, [messageList]);
+
   const handleFormSendMessage = (text) => {
-    const newMsg = {author: 'Вы', text: text};
+    const newMsg = {id: lastMessage().id + 1, author: 'Вы', text: text};
     setMessageList([...messageList, newMsg]);
   }
 
   useEffect( () => {
     const timeout = setTimeout(() => {
       if (messageList.length > 0) {
-        if (messageList[messageList.length - 1].author === 'Вы') {
-          const newMsg = {author: 'Бот', text: 'Оператор ответит Вам позже.'};
+        if (lastMessage().author === 'Вы') {
+          const newMsg = {id: lastMessage().id + 1, author: 'Бот', text: 'Оператор ответит Вам позже.'};
           setMessageList([...messageList, newMsg]);
         }
       }
@@ -24,13 +31,13 @@ const App = () => {
     return () => {
       clearTimeout(timeout);
     }
-  }, [messageList]);
+  }, [messageList, lastMessage]);
 
   return (
     <div className="app">
       <h1 className="app-header">Чат</h1>
       <div className="app-content">
-        {messageList.map(mes => <Message msg={mes} />)}
+        {messageList.map(mes => <Message msg={mes} key={mes.id} />)}
       </div>
       <Form change={handleFormSendMessage} className="app-footer" />
     </div>
