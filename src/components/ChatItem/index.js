7b56aa2ee8a3ee84fdaceb64e9ceac9ad4a512c4
Dmitry <dp_140482@mail.ru>
2021-11-17 11:42:15 +0300

    Чаты в store и селекторы

---
+++ src/components/ChatItem/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteChat } from "../../store/chats/actions";
+import { Link } from 'react-router-dom';
+import './ChatItem.css';
+
+const ChatItem = ({ chat, chatClass }) => {
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <li className= {chatClass}>
+                <button className="deleteBtn" onClick={
+                    () => {dispatch(deleteChat(chat.id))}
+                }>–</button>
+                <Link to={chat.id}>{chat.name}</Link>
+            </li>
+        </div>
+    );
+}
+
+export default ChatItem;
