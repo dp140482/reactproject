28d0a494b8733d92c0d3c49b39d15143df2107a6
Dmitry <dp_140482@mail.ru>
2021-11-24 21:52:34 +0300

    ДЗ 7 пп. 1, 2

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import { profileReducer } from "./profile/reducer";
 import { chatsReducer } from "./chats/reducer";
 import { messagesReducer } from "./messages/reducer";
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   combineReducers({
@@ -9,5 +12,5 @@ export const store = createStore(
     profile: profileReducer,
     messages: messagesReducer
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
